7d30ab058c107855eb42e565d8ec62d0ccd5b28d
JVRPascua <pascuavincentjohn@gmail.com>
2021-10-22 15:27:12 +0800

    Created a database connection file and an empty registration backend

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+//define named constants
+DEFINE('DB_HOST', 'localhost');
+DEFINE('DB_USER', 'root');
+DEFINE('DB_PASSWORD', '');
+DEFINE('DB_NAME', 'it15_database');
+
+//Establish connection
+$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+?>
